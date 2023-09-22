4066200e10997ae7cccf5a7896d0ddda254482f9
Sam Watts <swatts@ed.ac.uk>
2023-09-22 17:43:28 +0100

    add residual image paths

--- run_drusen_seg.py
+++ run_drusen_seg.py
@@ -8,7 +8,9 @@ MODEL_WEIGHTS = dict(
 
 DATASETS = dict(
     ODIR = 'data/ODIR_inference/',
-    STARE = 'data/STARE_inference/'
+    STARE = 'data/STARE_inference/',
+    ODIR_residual = "data/ODIR_inference_residual/",
+    STARE_residual = "data/STARE_inference_residual/",
 )
 
 
@@ -19,7 +21,9 @@ def generate_output_path(mode: str, dataset: str):
 def run_inference_notebooks(mode: dict, dataset: str):
     dataset_path = DATASETS[dataset]
     output_path = generate_output_path(mode, dataset)
-
+    intermediate_path = "outputs/intermediate/"
+    Path(intermediate_path).mkdir(parents=True, exist_ok=True)
+    
     # run whole image model
     pm.execute_notebook(
         "test_segmentation_Deep_lab.ipynb",
@@ -27,7 +31,7 @@ def run_inference_notebooks(mode: dict, dataset: str):
         parameters=dict(
             WEIGHTS_PATH=MODEL_WEIGHTS["image_level"], 
             PATH_DATA=dataset_path,
-            PATH_OUTPUT = 'outputs/intermediate/',
+            PATH_OUTPUT = intermediate_path,
         ),
         kernel="python",
         log_output=True,
@@ -43,18 +47,20 @@ def run_inference_notebooks(mode: dict, dataset: str):
         parameters=dict(
             WEIGHTS_PATH=MODEL_WEIGHTS["patch_level"], 
             INPUT_DIR_FULL_IMAGE=dataset_path,
-            INPUT_DIR_MASK_PRED_FULL_IMAGE = 'outputs/intermediate/', 
+            INPUT_DIR_MASK_PRED_FULL_IMAGE = intermediate_path, 
             OUTPUT_DIR_MASK_PRED_FULL_IMAGE=output_path
         ),
         kernel="python",
         log_output=True,
     )
     
-    [f.unlink() for f in Path('outputs/intermediate/').glob("*") if f.is_file()] 
+    [f.unlink() for f in Path(intermediate_path).glob("*") if f.is_file()] 
     
 def main():
     run_inference_notebooks("baseline", "STARE")
     run_inference_notebooks("baseline", "ODIR")
+    run_inference_notebooks("residual", "STARE_residual")
+    run_inference_notebooks("residual", "ODIR_residual")
 
     
 if __name__ == "__main__":
